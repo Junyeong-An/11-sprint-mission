d7a8e440e47fd8422205ed22db3663e1c45df01b
Junyeong-An <2117ab@gmail.com>
2026-03-07 16:20:46 +0900

    feat: 인터페이스 작성

---
+++ 11-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.Message;
+import java.util.List;
+import java.util.UUID;
+
+public interface MessageService {
+    Message createMessage(String content);
+    Message findMessage(UUID id);
+    List<Message> getAllMessages();
+    void updateContent(UUID id, String content);
+    void delete(UUID id);
+}
