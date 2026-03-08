4a01b9e0cf0b1f6733b45f69166ef17978247168
Junyeong-An <2117ab@gmail.com>
2026-03-08 21:26:59 +0900

    fix: 서비스 구현체를 FileService로 바꾸어 테스트

--- 11-sprint-mission/src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ 11-sprint-mission/src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -6,17 +6,17 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
-import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
-import com.sprint.mission.discodeit.service.jcf.JCFUserService;
+import com.sprint.mission.discodeit.service.file.FileChannelService;
+import com.sprint.mission.discodeit.service.file.FileMessageService;
+import com.sprint.mission.discodeit.service.file.FileUserService;
 import java.util.UUID;
 
 public class JavaApplication {
 
     public static void main(String[] args) {
-        UserService userService = new JCFUserService();
-        ChannelService channelService = new JCFChannelService();
-        MessageService messageService = new JCFMessageService(userService, channelService);
+        UserService userService = new FileUserService();
+        ChannelService channelService = new FileChannelService();
+        MessageService messageService = new FileMessageService();
 
         System.out.println("----- 유저 서비스 테스트 -----");
         testUserService(userService);
