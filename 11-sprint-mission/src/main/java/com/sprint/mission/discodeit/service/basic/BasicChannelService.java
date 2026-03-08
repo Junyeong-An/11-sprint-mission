18531704f4e72a37f327059b7d81d68c506e896c
Junyeong-An <2117ab@gmail.com>
2026-03-08 21:57:57 +0900

    Refactor: Basic*Service 구현체를 활용하여 테스트

---
+++ 11-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import java.util.List;
+import java.util.UUID;
+
+public class BasicChannelService implements ChannelService {
+    private final ChannelRepository channelRepository;
+
+    public BasicChannelService(ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
+    }
+
+    @Override
+    public Channel createChannel(String name) {
+        Channel channel = new Channel(name);
+        return channelRepository.save(channel);
+    }
+
+    @Override
+    public Channel findChannel(UUID id) {
+        return channelRepository.findById(id);
+    }
+
+    @Override
+    public List<Channel> getAllChannels() {
+        return channelRepository.findAll();
+    }
+
+    @Override
+    public void updateName(UUID id, String name) {
+        Channel channel = channelRepository.findById(id);
+        channel.update(name);
+        channelRepository.save(channel);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        channelRepository.deleteById(id);
+    }
+}
