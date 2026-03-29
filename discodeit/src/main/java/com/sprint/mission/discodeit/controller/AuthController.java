877613246b8c4e8cc5a3e1b5ed3d612808dac3a3
Junyeong-An <dev.jun0321@gmail.com>
2026-03-29 19:37:24 +0900

    feat: 권한 관리 컨트롤러 구현

---
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.dto.user.UserLoginRequest;
+import com.sprint.mission.discodeit.service.dto.user.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public UserResponse login(@RequestBody UserLoginRequest request) {
+        return authService.login(request);
+    }
+}
+
