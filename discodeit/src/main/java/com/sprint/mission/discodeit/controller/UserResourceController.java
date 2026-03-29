5ded4ac2205684c4a2d4a0d98f6b0423f78e2627
Junyeong-An <dev.jun0321@gmail.com>
2026-03-29 19:41:36 +0900

    feat: 유저상태 관리 컨트롤러 구현

---
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/UserResourceController.java
@@ -0,0 +1,38 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.controller.dto.UserDto;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.dto.user.UserResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserResourceController {
+    private final UserService userService;
+
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
+    public ResponseEntity<List<UserDto>> findAll() {
+        List<UserDto> users = userService.findAll().stream()
+                .map(this::toDto)
+                .toList();
+        return ResponseEntity.ok(users);
+    }
+
+    private UserDto toDto(UserResponse userResponse) {
+        return new UserDto(
+                userResponse.id(),
+                userResponse.createdAt(),
+                userResponse.updatedAt(),
+                userResponse.username(),
+                userResponse.email(),
+                userResponse.profileId(),
+                userResponse.online()
+        );
+    }
+}
